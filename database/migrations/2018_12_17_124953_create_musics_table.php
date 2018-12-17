75bfcd53e6a38de53c9cd5c99bb192e5e736e869
phphuong0912 <phphuong0912@gmail.com>
2018-12-17 21:08:00 +0700

    #3-edit3

---
+++ database/migrations/2018_12_17_124953_create_musics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('musics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('view_count');
+            $table->integer('artist_id');
+            $table->integer('artist_name');
+            $table->integer('album_id');
+            $table->integer('album_name');
+            $table->text('lyrics');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musics');
+    }
+}
