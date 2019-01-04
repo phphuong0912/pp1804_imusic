cc54bfab334fdb05bdc98ffac7cfee631049dd5a
caoxuanhoi <xuanhoi161995@gmail.com>
2019-01-04 20:28:19 +0700

    Feature/#5.0-front end

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+mix.style([
+   'bower_components/Font-Awesome/css/fontawesome.css',
+   'bower_components/bootstrap/dist/css/bootstrap.css',
+], 'public/css')
+.js('resources/sass/app.scss', 'public/js');
