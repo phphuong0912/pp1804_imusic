dcaea75b818def9a9d1f509dd83efd0e3dd40e58
phphuong0912 <phphuong0912@gmail.com>
2019-01-14 19:14:28 +0700

    welcome-4

---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,158 @@
+
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Nghe nhạc trực tuyến</title>
+<base href="{{asset('')}}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mosaic Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="source/css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="source/css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="source/css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<!-- lined-icons -->
+<link rel="stylesheet" href="source/css/icon-font.css" type='text/css' />
+<!-- //lined-icons -->
+ <!-- Meters graphs -->
+<script src="source/js/jquery-2.1.4.js"></script>
+
+
+</head> 
+    	 <!-- /w3layouts-agile -->
+ <body class="sticky-header left-side-collapsed"  onload="initMap()">
+    <section>
+      <!-- left side start-->
+		<div class="left-side sticky-left-side">
+
+			<!--logo and iconic logo start-->
+			<div class="logo">
+				<h1><a href="{{ route('trang-chu')}} ">Music.<span>F</span></a></h1>
+			</div>
+			<div class="logo-icon text-center">
+				<a href="{{ route('trang-chu')}} ">M </a>
+			</div>
+ 	 <!-- /w3l-agile -->
+			<!--logo and iconic logo end-->
+			<div class="left-side-inner">
+
+				<!--sidebar nav start-->
+					@include('admin.layout.menu')
+				<!--sidebar nav end-->
+			</div>
+		</div>
+		 	 <!-- /w3layouts-agile -->
+					<!-- app-->
+			<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog facebook" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body">
+							<div class="app-grids">
+								<div class="app">
+						<div class="col-md-5 app-left mpl">
+							<h3>Mosaic mobile app on your smartphone!</h3>
+							<p>Download and Avail Special Songs Videos and Audios.</p>
+							<div class="app-devices">
+								<h5>Gets the app from</h5>
+								<a href="#"><img src="images/1.png" alt=""></a>
+								<a href="#"><img src="images/2.png" alt=""></a>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-7 app-image">
+							<img src="images/apps.png" alt="">
+						</div>
+						<div class="clearfix"></div>
+					</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //app-->
+ 	 <!-- /w3l-agile -->
+		<!-- signup -->
+			<div class="modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content modal-info">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+						</div>
+						<div class="modal-body modal-spa">
+							<div class="sign-grids">
+								<div class="sign">
+									<div class="sign-left">
+										<ul>
+											<li><a class="fb" href="#"><i></i>Sign in with Facebook</a></li>
+											<li><a class="goog" href="#"><i></i>Sign in with Google</a></li>
+											<li><a class="linkin" href="#"><i></i>Sign in with Linkedin</a></li>
+										</ul>
+									</div>
+									<div class="sign-right">
+										<form action="#" method="post">
+
+											<h3>Create your account </h3>
+											<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+											<input type="text" value="Mobile number" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Mobile number';}" required="">
+											<input type="text" value="Email id" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email id';}" required="">	
+											<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" required="">	
+											
+											<input type="submit" value="CREATE ACCOUNT" >
+										</form>
+									</div>
+									<div class="clearfix"></div>								
+								</div>
+								<p>By logging in you agree to our <span>Terms and Conditions</span> and <span>Privacy Policy</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- //signup -->
+ 	 <!-- /w3l-agile -->
+		<!-- left side end-->
+		<!-- main content start-->
+		<div class="main-content">
+			<!-- header-starts -->
+			<div class="header-section">
+			<!--toggle button start-->
+			<a class="toggle-btn  menu-collapsed"><i class="fa fa-bars"></i></a>
+			<!--toggle button end-->
+			<!--notification menu start -->
+			<div class="menu-right">
+			@include('admin.layout.header')
+</div>
+			<!--notification menu end -->
+			<!-- //header-ends -->
+ 	 <!-- /w3l-agileits -->
+				@yield('content')					
+				@include('admin.layout.footer')					
+        <!--footer section start-->
+			<footer>
+			   <p>&copy 2019 Music.Framgia | Design by Phạm Hoàng Phương-Cao Xuân Hợi</p>
+			</footer>
+        <!--footer section end-->
+ 	 <!-- /w3l-agile -->
+      <!-- main content end-->
+   </section>
+  
+<script src="source/js/jquery.nicescroll.js"></script>
+<script src="source/js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="source/js/bootstrap.js"></script>
+</body>
+</html>
